c57c874d18992f4582a52e10c253e9a461ece6a6
zhanglonglong <zhanglonglong@odaily.email>
2024-01-31 13:31:26 +0800

    aa

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import subprocess
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(title="fastvpn")
+
+def ping(host):
+  ret = subprocess.run(
+    ['ping', '-c', '3', host],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+  )
+  return ret.returncode == 0
+
+def write_config(appendinfo):
+  wpath = os.getcwd()
+  config_file = f'{wpath}/privoxy_config'
+  source = f'{wpath}/privoxy_config_example'
+  shutil.copy(source, config_file)
+  with open(config_file,"a") as variable_name:
+    variable_name.write(appendinfo)
+
+
+
+@app.get("/hostapi")
+async def say_hello(host: str,port:str,auth:str):
+  is_connect = ping(host);
+  if is_connect:
+    write_config(f"forward-socks5   /               {auth}@{host}:{port}  .")
+    # 重启
+    path = os.getcwd()
+    kill_status = os.system(" ps -ef|grep privoxy| grep -v grep |awk '{print $2}' |xargs kill -9 ")
+    # start_privoxy= os.system(f"/usr/sbin/privoxy  {path}/privoxy_config")
+    start_privoxy= os.system(f"/usr/local/sbin/privoxy  {path}/privoxy_config")
+    return  ping(host);
+  else:
+    return is_connect;
+
+
+if __name__ == '__main__':
+  """
+  """
+  uvicorn.run(app='main:app',host="0.0.0.0",port=8090,reload=True)
